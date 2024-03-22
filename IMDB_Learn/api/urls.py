bca12f4823264b8e034d47acb9f8aee3814664b7
Ronaldyika <buhnyuyronald@gmail.com>
2024-03-23 00:30:09 +0100

    explored the django jsonget

--- IMDB_Learn/api/urls.py
+++ IMDB_Learn/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import movieList
+from .views import movieList,movieDetail
 
 urlpatterns = [
     path('',movieList,name='movielist'),
+    path('<int:pk>/',movieDetail,name='moviedetail'),
     
 ]
--- IMDB_Learn/api/views.py
+++ IMDB_Learn/api/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from .models import Movie
+from rest_framework import response
+from django.http import JsonResponse
 
 def movieList(request):
     queryset = Movie.objects.all()
-    movies = list(queryset.values())
+    data = {
+        'movies':list(queryset.values())
+    }
 
-    return({'movies':movies})
+
+    return JsonResponse(data)
+
+def movieDetail(request, pk):
+    queryset = Movie.objects.get(pk=pk)
+    data = {
+        'name': queryset.name,
+        'description':queryset.description,
+        'isActive':queryset.isActive,
+    }
+
+    return JsonResponse(data)
